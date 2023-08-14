7edd133046eb159a12f4c8d5c2633270c783e84f
Tolulope <emryzs01@gmail.com>
2023-08-15 01:55:16 +0300

    trash

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
-import SideBar from "@/components/SideBar";
-import { Button } from "@/components/ui/Button";
-import Image from "next/image";
-import Link from "next/link";
+// import { Button } from "@/components/ui/Button";
+// import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="p-0 m-0 font-bold text-[25px]">
-      Welcome to the ultimate home page of the fucking sticker
+      {/* Welcome to the ultimate home page of the fucking sticker
       <br />
       <Button variant="link">
         <Link href="/profile">Go to Profile</Link>
@@ -14,7 +12,7 @@ export default function Home() {
       |
       <Button variant="link">
         <Link href="/login">Go to Login</Link>
-      </Button>
+      </Button> */}
     </div>
   );
 }
